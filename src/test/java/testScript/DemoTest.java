0e4c513e086c391ba32fa2353900c1d9d72bfc4f
Manuel Morales <usuario@MacBook-Pro-de-Usuario.local>
2025-03-10 08:10:30 -0500

    start project

--- src/test/java/testScript/DemoTest.java
+++ src/test/java/testScript/DemoTest.java
@@ -2,17 +2,10 @@ package testScript;
 
 import baseClass.BaseClass;
 import org.testng.annotations.Test;
-import pages.DemoPage;
 
 public class DemoTest extends BaseClass {
     @Test
     public void demoTestUno()throws Exception{
-        DemoPage dm = new DemoPage(driver);
-        System.out.println("Arrancamos Test....");
-
-        Thread.sleep(1000);
-        dm.searchBox("facebook");
-        Thread.sleep(2000);
     }
 
 }
